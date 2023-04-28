0c45959b7f8f60a9c5e8b771abe6255b6b6708ee
Marcela Silva <marcela.ops@hotmail.com>
2023-04-28 10:40:21 -0300

    `newUser.php`

---
+++ newUser.php
@@ -0,0 +1,18 @@
+<h1>Novo Usuário</h1>
+
+<form action="?page=save" method="POST">
+  <input type="hidden" name="action" value="post">
+  <div class="mb-3">
+    <label>Nome</label>
+    <input type="text" name="name" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label>Email</label>
+    <input type="email" name="email" class="form-control">
+  </div>
+  <form>
+  <div class="mb-3">
+    <button type="submit" class="btn btn-primary">Enviar</button>
+  </div>
+</form>
+</form>
