6e13227f90d124b1915f1553415cc7b517d6ed02
Marcela Silva <marcela.ops@hotmail.com>
2023-04-28 10:39:42 -0300

    `index.php`

--- index.php
+++ index.php
@@ -1,3 +1,69 @@
-<?php
-echo "Initial commit";
-// nova branch
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Cadastro</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body class="p-3 m-0 border-0 bd-example">
+    
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Cadastro</a>
+        <button 
+          class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=new
+              ">Novo Usuário</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=list">Lista de Usuários</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <div class="container">
+      <div class="row">
+
+        <?php
+          include("config.php");
+
+          switch(@$_REQUEST["page"]) {
+            case "new":
+              include("newUser.php");
+            break;
+            case "list":
+              include("userList.php");
+            break;
+            case "save":
+              include("postUsers.php");
+            break;
+            case "update":
+              include("updateUser.php");
+            break;
+
+            default:
+              print "Bem vindo, cadastre um novo usuário.";
+          }
+        ?>
+      </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+  </body>
+</html>
